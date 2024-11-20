cac2d6349cb4b9e3ae847d563655d3e4ad0da3df
Khaled.Mohamed <khaled.mohammed@intercom.com.eg>
2024-11-20 19:36:48 +0200

    removed unused code

--- src/main/java/com/banquemisr/challenge05/taskMangager/controlers/AuthController.java
+++ src/main/java/com/banquemisr/challenge05/taskMangager/controlers/AuthController.java
@@ -2,7 +2,6 @@ package com.banquemisr.challenge05.taskMangager.controlers;
 
 import com.banquemisr.challenge05.taskMangager.entity.AuthenticationRequest;
 import com.banquemisr.challenge05.taskMangager.entity.AuthenticationResponce;
-import com.banquemisr.challenge05.taskMangager.entity.RegisterRequest;
 import com.banquemisr.challenge05.taskMangager.entity.User;
 import com.banquemisr.challenge05.taskMangager.services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
